2b0d5188158919986a600efb2b47c16c5c1e4688
Fuad Jamal <jamalfuad34@gmail.com>
2025-04-30 09:38:38 +0200

    configuring the derivatives for tailwind

---
+++ my-tail/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <h1 className="text-red-400">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
