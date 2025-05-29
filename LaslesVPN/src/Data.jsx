24367e467fe9e672f21f4cc7cc1bb7a3afc2292b
Fuad Jamal <jamalfuad34@gmail.com>
2025-05-29 10:18:22 +0200

    sizing the info

--- LaslesVPN/src/Data.jsx
+++ LaslesVPN/src/Data.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import user from './assets/user.svg'
+import location from './assets/location.png'
+import services from './assets/Server.png'
 
 export default function Data () {
   return (
-    <div>
+    <div className='flex flex-row mt-10 text-center w-full max-w-3xl mx-auto justify-between gap-10'>
 
-    <div>90+<br/>Users</div>
-    <div>30+<br/>Locations</div>
-    <div>50+<br/>Services</div>
+    <div className='border-r'><img src={user}></img><b>90+</b><br/>Users</div>
+    <div><img src={location}></img><b>30+</b><br/>Locations</div>
+    <div><img src={services}></img><b>50+</b><br/>Services</div>
     
     </div>
   )
