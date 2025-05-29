7b328cf3ffd86f54cad8e44d37dbd4c6f9cb033e
Fuad Jamal <jamalfuad34@gmail.com>
2025-05-29 11:27:27 +0200

    importing components

---
+++ LaslesVPN/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Navbar from './Navbar.jsx'
+import Started  from './Started.jsx'
+import Data from './Data.jsx'
+import Provide from './provide.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <center>
+  <Navbar />
+  <Started />
+  <Data />
+  <Provide />
+    </center>
+  )
+}
+
+export default App
