f653a2d7bb41851f3fd9434c4c3c5dce458935c0
Fuad Jamal <jamalfuad34@gmail.com>
2025-05-29 11:24:55 +0200

    continuing to next div of project

---
+++ LaslesVPN/src/provide.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import illustration from './assets/illustration 2.svg'
+
+export default function Provide() {
+  return (
+    <div>
+        <div><img src={illustration}></img></div>
+    </div>
+  )
+}
