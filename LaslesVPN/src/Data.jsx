aa4ea8986506bc8608ccf831b0536be976c0f99e
Fuad Jamal <jamalfuad34@gmail.com>
2025-05-29 08:56:46 +0200

    inputting data info

---
+++ LaslesVPN/src/Data.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Data () {
+  return (
+    <div>
+
+    <div>90+<br/>Users</div>
+    <div>30+<br/>Locations</div>
+    <div>50+<br/>Services</div>
+    
+    </div>
+  )
+}
